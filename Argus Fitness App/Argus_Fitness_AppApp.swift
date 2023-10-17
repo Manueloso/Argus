d9d825d384ea76659ae1caa3f4cb9b3ed37faca9
xHero <loergame@gmail.com>
2023-10-17 11:38:11 +0200

    Initial Commit

---
+++ Argus Fitness App/Argus_Fitness_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Argus_Fitness_AppApp.swift
+//  Argus Fitness App
+//
+//  Created by Carmine Andreozzi on 17/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Argus_Fitness_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
